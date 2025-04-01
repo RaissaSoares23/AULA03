67a70da1dc92bff54ffe7b2c7880d55a1824f599
raissa soares <raissasoaresandradedasilva@gmail.com>
2025-04-01 15:45:47 -0300

    atualização no codigo

---
+++ exercicio04.py
@@ -0,0 +1,6 @@
+n1 = 6
+n2 = 10
+if n1 > n2:
+    print ("n1 é o maior")
+else:
+    print ("n2 é o maior")
---
+++ exercico05.py
@@ -0,0 +1,7 @@
+n1 = int(input ("Digite o primeiro numero"))
+n2 = int (input ("Digite o segundo numero"))
+
+if n1>n2:
+    print ("Escreva n1 primeiro", n1, n2)
+else:
+    print ("Escreva n2 primeiro", n2, n1)
