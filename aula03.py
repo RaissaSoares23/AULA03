718afba527525febe8cce7373816b7cb77c4d809
raissa soares <raissasoaresandradedasilva@gmail.com>
2025-04-01 14:19:38 -0300

    atualização no codigo

--- aula03.py
+++ aula03.py
@@ -1,4 +1,9 @@
+from turtledemo.sorting_animate import start_isort
+
 nome = input ("Qaul seu nome:")
 idade = input ("Qual a dua idade:")
 salario = float (input("Qual o seu salário:") )
-print ( nome,idade,salario)
+aumento = float (input("De quanto foi o seu aumento:"))
+total = (salario * aumento)/100
+
+print ( nome,idade,salario+total)
