3b2fb7af720fdac894a2b1d2ffc8f9735aeddd0b
raissa soares <raissasoaresandradedasilva@gmail.com>
2025-04-01 15:56:18 -0300

    atualização no codigo

---
+++ exercicio06.py
@@ -0,0 +1,10 @@
+nota1 = float (input("Digite nota1:"))
+nota2 = float (input("Digite nota2:"))
+nota3 = float (input("Digite nota3:"))
+
+media = (nota1+nota2+nota3)/3
+
+if media >= 7:
+    print("Aluno aprovado")
+else:
+    print ("Aluno reprovado")
