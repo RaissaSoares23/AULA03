ec62fe17dd2edd0b5e19fbbb5efa2dd57acf2971
raissa soares <raissasoaresandradedasilva@gmail.com>
2025-04-03 14:28:47 -0300

    novo axercicio

--- exercicio08.py
+++ exercicio08.py
@@ -1,14 +1,18 @@
 g= 5.8
 e=4.9
+total= 0
 Combustivel = input ("Qual tipo de combustivel você abasteceu:\n"
-                     "g para gasolina\n"
+                     "g  para gasolina\n"
                      "e para etanoln")
 litros = float (input ("Quantos litros você abasteceu:"))
 
-if Combustivel == "g":
+if Combustivel == "g" or Combustivel == "G":
     total = litros*g
-else:
+
+elif Combustivel == "e" or Combustivel == "E":
     total = litros*e
+else:
+    print ("Tipo de combustivel invalido!")
 
 print (f"você gastou: R${total}")
 
