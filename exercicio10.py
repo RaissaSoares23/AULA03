9207d037f7ebcdf0e6f6723240636cb37276b985
raissa soares <raissasoaresandradedasilva@gmail.com>
2025-04-08 14:35:53 -0300

    novo exercicio

---
+++ exercicio10.py
@@ -0,0 +1,42 @@
+num = int (input("Informe um numero entre 1 e 12:"))
+
+if num == 1:
+    print ("O numero",num," é referente ao mês de Janeiro")
+
+elif num == 2:
+    print ("O numero",num," é referente ao mês de Fevereiro")
+
+elif num == 3:
+    print ("O numero",num," é referente ao mês de Março")
+
+elif num == 4:
+    print ("O numero",num," é referente ao mês de Abril")
+
+elif num == 5:
+    print ("O numero",num," é referente ao mês de Maio")
+
+elif num == 6:
+    print ("O numero",num," é referente ao mês de Junho")
+
+elif num == 7:
+    print ("O numero",num," é referente ao mês de Julho")
+
+elif num == 8:
+    print ("O numero",num," é referente ao mês de Agosto")
+
+elif num == 9:
+    print ("O numero",num," é referente ao mês de Setembro")
+
+elif num == 10:
+    print ("O numero",num," é referente ao mês de Outubro")
+
+elif num == 11:
+    print ("O numero",num," é referente ao mês de Novembro")
+
+elif num == 12:
+    print ("O numero",num," é referente ao mês de Dezembro")
+
+else:
+    print ("Número inválido")
+
+
